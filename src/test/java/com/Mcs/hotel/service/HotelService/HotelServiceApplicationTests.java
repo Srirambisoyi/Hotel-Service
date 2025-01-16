22bae31e98f6d2258859b5ee56a691db544431fb
Sriram Bisoyi <bisoyisriram@gmail.com>
2025-01-16 15:17:59 +0530

    HotelService added

---
+++ src/test/java/com/Mcs/hotel/service/HotelService/HotelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mcs.hotel.service.HotelService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
